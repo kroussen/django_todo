725cb690a0a5ebea0e968391b53421f400a9a929
Kroussen Paws <kroussen.322@gmail.com>
2024-03-03 00:38:16 +0500

    Added model Task

--- ToDoList/app/models.py
+++ ToDoList/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название задачи')
+    description = models.TextField(verbose_name='Описание задачи')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания задачи')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования задачи')
+    completed_at = models.DateTimeField(auto_now=True, verbose_name='Дата завершения задачи')
+    due_date = models.DateField(blank=True, null=True)
+    completed = models.BooleanField(default=False, verbose_name='Завершена?')
+
+    PRIORITY_CHOICES = [
+        ('низкий', 'Низкий'),
+        ('средний', 'Средний'),
+        ('высокий', 'Высокий'),
+    ]
+
+    priority = models.CharField(max_length=100, choices=PRIORITY_CHOICES, default='средний', verbose_name='Приоритет')
+
+    def __str__(self):
+        return f"{self.title} - {self.created_at}: {self.completed}"
